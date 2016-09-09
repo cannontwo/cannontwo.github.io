4053a96e9e73e3a3a3e7f2614249c0c2ecbbd06d
cannon <cannon10100@gmail.com>
2016-09-08 20:09:08 -0500

    Added dates

--- chart.js
+++ chart.js
@@ -3,7 +3,7 @@ google.charts.setOnLoadCallback(drawChart);
 
 function drawChart() {
   var data = new google.visualization.DataTable();
-  data.addColumn('number', 'Month (MMYYYY)');
+  data.addColumn('date', 'Month (MMYYYY)');
   data.addColumn('number', 'Books bought');
   data.addColumn('number', 'Books read');
   data.addColumn({type: 'string', role: 'annotation'})
@@ -11,13 +11,16 @@ function drawChart() {
 
   // Add book reading data here.
   data.addRows([
-    [072016, 0, 0, '', 'Start!'],
-    [082016, 19, 1, '', 'Read: The Three Stigmata of Palmer Eldritch']
+    [new Date(2016, 7), 0, 0, '', 'Start!'],
+    [new Date(2016, 8), 19, 1, '', 'Read: The Three Stigmata of Palmer Eldritch']
   ]);
 
   var options = {
     chart: {
       subtitle: 'Sometimes I buy too many books'
+    },
+    hAxis: {
+      format: 'MMYYYY'
     },
     height: 300
   };
